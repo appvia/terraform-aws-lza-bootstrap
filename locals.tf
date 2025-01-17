12b1f94b27f16fd4987759d5946cf1e7ae090a4c
Richard Jones <richard.jones@appvia.io>
2025-01-17 17:05:28 +0000

    feat(modules): Fix tag and cfn substitution errors

--- locals.tf
+++ locals.tf
@@ -2,4 +2,35 @@
 locals {
   ## Organizational root id
   root_id = data.aws_organizations_organization.current.roots[0].id
+
+    ## Stack capabilities
+  capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
+
+  ## Terraform State StackSet Parameters
+  terraform_state_parameters = {}
+
+  ## Parameters for the OIDC provider stackset
+  oidc_provider_parameters = {
+    ClientIdList         = join(",", var.oidc_provider_client_ids)
+    IdentityProviderName = var.oidc_provider_name
+    ThumbprintList       = join(",", var.oidc_provider_thumbprints)
+  }
+
+  ## Parameters for the IAM roles stackset
+  iam_roles_parameters = {
+    CloudAccessRoleReadOnlyName  = var.cloudaccess_role_readonly_name
+    CloudAccessRoleReadWriteName = var.cloudaccess_role_readwrite_name
+    IdentityProviderName         = var.oidc_provider_name
+    RepositoryName               = var.cloudaccess_repository_name
+    TerraformStateKey            = var.cloudaccess_terraform_state_key
+    TerraformStateROPolicyName   = var.cloudaccess_terraform_state_readonly_policy_name
+    TerraformStateRWPolicyName   = var.cloudaccess_terraform_state_readwrite_policy_name
+  }
+
+  ## Tags applied to the stackset and the resources it creates
+  tags = merge(var.tags, {
+    git_repository = var.git_repository,
+    enviroment = var.environment,
+    owner = var.owner
+  }) 
 }
